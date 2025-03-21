07ce99544a21365ca82ac7bf75820aa392d814e4
ku9nov <sergeyku9nov@gmail.com>
2025-03-21 13:20:02 +0200

    finish first preparation for using traefik by default and parse labels to dynamic yaml file by default

---
+++ debug_traefik_conf_generation.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+DOCKER_COMPOSE_FILE="docker-compose.rolling.yaml"
+
+# Getting list of services, ignoring services which contains "proxy" in name
+services=$(yq eval '.services | keys | map(select(test("proxy") | not)) | .[]' "$DOCKER_COMPOSE_FILE")
+
+service_ips=""
+
+# Getting containers IP-addresses
+for service in $services; do
+  container_ids=$(docker compose -f "$DOCKER_COMPOSE_FILE" ps -q "$service")
+
+  for container_id in $container_ids; do
+    container_ip=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' "$container_id")
+    if [ -n "$container_ip" ]; then
+      service_ips="${service_ips}${service}:${container_ip} "
+    fi
+  done
+done
+
+if [ -z "$service_ips" ]; then
+  echo "Error: The list of IP addresses is empty!"
+  exit 1
+fi
+
+# Get Traefik-labels and generate YAML
+docker ps -q | xargs -I {} docker inspect --format '{{json .Config.Labels}}' {} | jq -s --arg service_ips "$service_ips" '
+  def split_ips:
+    reduce ($service_ips | split(" "))[] as $item ({}; 
+      if ($item | contains(":")) then
+        . + { ($item | split(":")[0]): (.[$item | split(":")[0]] + [ $item | split(":")[1] ] // []) }
+      else . end
+    );
+
+  def extract_health_check(item):
+    if (
+      (item["traefik.http.services." + item["com.docker.compose.service"] + ".loadbalancer.healthCheck.path"] // "") == "" and
+      (item["traefik.http.services." + item["com.docker.compose.service"] + ".loadbalancer.healthCheck.interval"] // "") == "" and
+      (item["traefik.http.services." + item["com.docker.compose.service"] + ".loadbalancer.healthCheck.timeout"] // "") == ""
+    ) then
+      empty
+    else
+      {
+        path: (item["traefik.http.services." + item["com.docker.compose.service"] + ".loadbalancer.healthCheck.path"] // ""),
+        interval: (item["traefik.http.services." + item["com.docker.compose.service"] + ".loadbalancer.healthCheck.interval"] // ""),
+        timeout: (item["traefik.http.services." + item["com.docker.compose.service"] + ".loadbalancer.healthCheck.timeout"] // "")
+      }
+    end;
+
+
+  reduce .[] as $item ({}; 
+    if ($item["com.docker.compose.service"] | in(split_ips)) then
+      # Debug: Print all labels
+      debug("Service: \($item["com.docker.compose.service"])", ""),
+      debug("Labels: \($item | tostring)", ""),
+      debug("Rule: \($item["traefik.http.routers.\($item["com.docker.compose.service"]).rule"] | tostring)", ""),
+
+      .http.routers[$item["com.docker.compose.service"]].rule = $item["traefik.http.routers.\($item["com.docker.compose.service"]).rule"] // null |
+      .http.routers[$item["com.docker.compose.service"]].service = $item["com.docker.compose.service"] |
+      .http.services[$item["com.docker.compose.service"]].loadBalancer.servers = 
+        (split_ips[$item["com.docker.compose.service"]] | map({url: ("http://" + . + ":" + ($item["traefik.http.services.\($item["com.docker.compose.service"]).loadbalancer.server.port"] // "80"))})) |
+      
+      # Debug: Print values for HealthCheck Path, Interval and Timeout
+      debug("HealthCheck Path: \($item["traefik.http.services." + $item["com.docker.compose.service"] + ".loadbalancer.healthCheck.path"] // "none")", ""),
+      debug("HealthCheck Interval: \($item["traefik.http.services." + $item["com.docker.compose.service"] + ".loadbalancer.healthCheck.interval"] // "none")", ""),
+      debug("HealthCheck Timeout: \($item["traefik.http.services." + $item["com.docker.compose.service"] + ".loadbalancer.healthCheck.timeout"] // "none")", ""),
+
+      # Debug: Check labels for each element
+      debug("Calling extract_health_check for service: \($item["com.docker.compose.service"])", ""),
+      (extract_health_check($item) as $hc |
+        if $hc then
+          .http.services[$item["com.docker.compose.service"]].loadBalancer += { healthCheck: $hc }
+        else
+          .
+        end
+      )
+
+
+    else . end
+  )' | yq -P 
+
+
+
+
